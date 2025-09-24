8a7670be96d27d1c9ec8af563d728f796da9d31d
Onkar Rane <onkarrane60@gmail.com>
2025-09-25 02:28:26 +0530

    Refactor Prisma client export and ensure global instance is set in non-production environments

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -10,6 +10,8 @@ declare global {
 
 const prisma: ReturnType<typeof prismaClientSingleton> = globalThis.prismaGlobal ?? prismaClientSingleton()
 
-export default prisma
-
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
+export default prisma;
+export { PrismaClient }
+export type { PrismaClient as PrismaClientType }
